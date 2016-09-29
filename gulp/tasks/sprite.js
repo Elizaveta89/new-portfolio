596bb662b18196ce66a849a931c935634953b4e8
Elizaveta <eovinova89@gmail.com>
2016-09-29 17:07:11 +0300

    Страница Welcome

---
+++ gulp/tasks/sprite.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function() {
+    $.gulp.task('sprite', function () {
+        var spriteData = $.gulp.src('source/img/icons/*.png')
+            .pipe($.gp.spritesmith({
+                imgName: 'img/sprite.png',
+                cssName: 'css/sprite.css'
+            }));
+        return spriteData.pipe($.gulp.dest($.config.root + '/assets'));
+    });
+};
