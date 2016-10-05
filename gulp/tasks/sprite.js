c3eddae7f3456710281633127d149519b92d5ad0
Elizaveta <eovinova89@gmail.com>
2016-10-05 19:57:45 +0300

    Верстака

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -5,7 +5,8 @@ module.exports = function() {
         var spriteData = $.gulp.src('source/img/icons/*.png')
             .pipe($.gp.spritesmith({
                 imgName: 'img/sprite.png',
-                cssName: 'css/sprite.css'
+                cssName: 'css/sprite.css',
+                padding: 30
             }));
         return spriteData.pipe($.gulp.dest($.config.root + '/assets'));
     });
